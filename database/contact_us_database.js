68a0214162b0ef9c806e8224cb17a673a16aa785
SudoAjay <sudoajay@gmail.com>
2023-10-10 14:50:36 +0530

    changes

--- database/contact_us_database.js
+++ database/contact_us_database.js
@@ -8,7 +8,7 @@ const pool = mysql
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: "fitness_plan_database",
+    database: process.env.MYSQL_DATABASE,
   })
   .promise();
 
